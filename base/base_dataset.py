ccb6df5d9b93952c5753296e9ab632f8b81acb4d
Tomas Berger <tomashimberg.berger@technipfmc.com>
2024-12-06 21:08:29 +0100

    Add preprocess as a dataset function

--- base/base_dataset.py
+++ base/base_dataset.py
@@ -15,6 +15,13 @@ class BaseDataset(ABC):
         print(f"No download method implemented for {self.name}")
         return 1
 
+    # Optional preprocess method (some datasets might need to preprocess the data to make it fit the 31 bands)
+    def preprocess(self):
+        print(f"No preprocess method implemented for {self.name}")
+        return 1
+
     @abstractmethod
+    # This method should return the next image in the dataset and the corresponding ground truth
+    # The shape of the image should be (3, H, W) and the shape of the ground truth should be (31, H, W)
     def get_next_img(self):
         pass
