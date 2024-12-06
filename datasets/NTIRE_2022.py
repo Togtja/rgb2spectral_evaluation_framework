aa8f9864c7723ba49e4417c6b302bf1a2c743a05
Tomas Berger <tomashimberg.berger@technipfmc.com>
2024-12-06 21:08:10 +0100

    Improve NTIRE_2022 download

--- datasets/NTIRE_2022.py
+++ datasets/NTIRE_2022.py
@@ -13,57 +13,42 @@ class NTIRE_2022(BaseDataset):
         super().__init__("NTIRE_2022", dataset_path)
 
     def download_dataset(self):
-        DATASET_PATH = f"./datasets/{self.name}"
+        DATASET_PATH = self.dataset_path
+
         # Download the dataset from the internet
         # if not os.path.exists(DATASET_PATH):
         #    os.makedirs(DATASET_PATH)
         # else:
         #    print(f"Dataset {self.name} already exists. Skipping download.")
         #    return 0
-        TRAIN_SPECTRAL_URL = (
-            "https://drive.google.com/file/d/1FQBfDd248dCKClR-BpX5V2drSbeyhKcq/view"
-        )
-        TRAIN_RGB_URL = (
-            "https://drive.google.com/file/d/1A4GUXhVc5k5d_79gNvokEtVPG290qVkd/view"
-        )
-        VALIDATION_SPECTRAL_URL = (
-            "https://drive.google.com/file/d/12QY8LHab3gzljZc3V6UyHgBee48wh9un/view"
-        )
-        VALIDATION_RGB_URL = (
-            "https://drive.google.com/file/d/19vBR_8Il1qcaEZsK42aGfvg5lCuvLh1A/view"
-        )
-        # TEST_RGB_URL = (
-        #    "https://drive.google.com/file/d/1A5309Gk7kNFI-ORyADueiPOCMQNTA7r5/view"
-        # )
+        google_downloads = {
+            "TRAIN_SPECTRAL": "https://drive.google.com/file/d/1FQBfDd248dCKClR-BpX5V2drSbeyhKcq/view",
+            "TRAIN_RGB": "https://drive.google.com/file/d/1A4GUXhVc5k5d_79gNvokEtVPG290qVkd/view",
+            "VALIDATION_SPECTRAL": "https://drive.google.com/file/d/12QY8LHab3gzljZc3V6UyHgBee48wh9un/view",
+            "VALIDATION_RGB": "https://drive.google.com/file/d/19vBR_8Il1qcaEZsK42aGfvg5lCuvLh1A/view",
+        }
 
-        # utils.download_file_from_google_drive(
-        #    TRAIN_SPECTRAL_URL, f"{DATASET_PATH}/train_spectral.zip"
-        # )
-        # utils.download_file_from_google_drive(
-        #    TRAIN_RGB_URL, f"{DATASET_PATH}/train_rgb.zip"
-        # )
-        # utils.download_file_from_google_drive(
-        #    VALIDATION_SPECTRAL_URL, f"{DATASET_PATH}/validation_spectral.zip"
-        # )
-        utils.download_file_from_google_drive(
-            VALIDATION_RGB_URL, f"{DATASET_PATH}/validation_rgb.zip"
-        )
-        # utils.download_file_from_google_drive(
-        #    TEST_RGB_URL, f"{DATASET_PATH}/test_rgb.zip"
-        # )
-
-        # Unzip the files
-        # For each file in the folder, unzip it
-        for file in os.listdir(DATASET_PATH):
-            if file.endswith(".zip"):
-                zip_file = DATASET_PATH + "/" + file
+        train_rgb_directory = DATASET_PATH + "/Train_RGB"
+        train_spec_directory = DATASET_PATH + "/Train_Spec"
+        os.makedirs(train_spec_directory, exist_ok=True)
+        for file_name, url in google_downloads.items():
+            zip_file = f"{DATASET_PATH}/{file_name}.zip"
+            if not os.path.exists(zip_file):
+                utils.download_file_from_google_drive(url, zip_file)
+            # Unzip the files
+            if "SPECTRAL" in file_name:
+                utils.unzip_file(zip_file, train_spec_directory)
+            else:
                 utils.unzip_file(zip_file, DATASET_PATH)
-                os.remove(zip_file)
-        VALID_RGB = "Valid_RGB"
-        VALID_SPEC = "Valid_Spec"
 
-        self.dataset_path = DATASET_PATH
-
+            # Move all .jpg files in the validation folder to the Train_RGB folder
+            if "VALIDATION_RGB" in file_name:
+                validation_rgb_directory = DATASET_PATH + "/Valid_RGB"
+                for root, dirs, files in os.walk(validation_rgb_directory):
+                    for file in files:
+                        if file.endswith(".jpg"):
+                            os.rename(f"{root}/{file}", f"{train_rgb_directory}/{file}")
+                os.rmdir(validation_rgb_directory)
         return 0
 
     def get_next_img(self):
@@ -73,7 +58,6 @@ class NTIRE_2022(BaseDataset):
         for file in os.listdir(train_rgb_directory):
             matlab_file = file.replace(".jpg", ".mat")
             matlab_file = train_spec_directory + "/" + matlab_file
-            # print(f"Processing {matlab_file}")
             try:
                 valid_img = load_matlab_file(matlab_file)
                 # Transpose valid_img to match model_prediction shape
